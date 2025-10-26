4dada30f329950eb5123ff4bbb39622b4aee908b
MukeshChoudhury-coder <mukeshchoudhury61@gmail.com>
2025-10-26 12:48:28 +0530

    Update script.js

--- Chumbak-clone-website/Chumbak-frontend-backend/frontend/script.js
+++ Chumbak-clone-website/Chumbak-frontend-backend/frontend/script.js
@@ -9,6 +9,88 @@ const addCartIconPara=document.querySelector(".add-cart-icon-p")
 
 
 /** stored products collection */
+const giftSection=[
+{
+    id:11,
+        name:"Bestseller collection",
+        img1: "/Chumbak-frontend-backend/chumbal-images/gift-page-pic-1.webp",
+       title:`Royal Hoot Owl Shape Cushion 13"x8" | Palace Memoirs`,
+       price: 1999
+
+},
+{
+    id:12,
+        name:"Bestseller collection",
+        img1: "/Chumbak-frontend-backend/chumbal-images/gift-page-pic-2.webp",
+       title:`Best. Sis. Ever. Gift Set of 3`,
+       price: 1499
+
+},
+{
+    id:13,
+        name:"Bestseller collection",
+        img1: "/Chumbak-frontend-backend/chumbal-images/gift-page-pic-3.webp",
+       title:`I love you from Head Message Card`,
+       price: 20
+
+},
+{
+    id:14,
+        name:"Bestseller collection",
+        img1: "/Chumbak-frontend-backend/chumbal-images/gift-page-pic-4.webp",
+       title:`Dolphin Mug - D`,
+       price: 562
+
+},
+{
+    id:15,
+        name:"Bestseller collection",
+        img1: "/Chumbak-frontend-backend/chumbal-images/gift-page-pic-5.webp",
+       title:`Grapefruit & Patchouli Soy Wax Candle - Small`,
+       price: 636
+
+},
+{
+    id:16,
+        name:"Bestseller collection",
+        img1: "/Chumbak-frontend-backend/chumbal-images/gift-page-pic-6.webp",
+       title:`Rani Bagh Gift Box - Rectangle | Palace Memoirs`,
+       price: 299
+
+},
+{
+    id:17,
+        name:"Bestseller collection",
+        img1: "/Chumbak-frontend-backend/chumbal-images/gift-page-pic-7.webp",
+       title:`Sweet Soiree Eau de Parfume 20ml`,
+       price: 599
+
+},
+{
+    id:18,
+        name:"Bestseller collection",
+        img1: "/Chumbak-frontend-backend/chumbal-images/gift-page-pic-8.webp",
+       title:`Festive Warmth and Joy E-Gift Card`,
+       price: 1000
+
+},
+{
+    id:19,
+        name:"Bestseller collection",
+        img1: "/Chumbak-frontend-backend/chumbal-images/gift-page-pic-9.webp",
+       title:`Aztec Cats Insulated Lunch Box`,
+       price: 1000
+
+},
+{
+    id:20,
+        name:"Bestseller collection",
+        img1: "/Chumbak-frontend-backend/chumbal-images/gift-page-pic-10.webp",
+       title:`Wrist Wonders Duo, Gift Set of 2 |  Comes in a Gift Bag`,
+       price: 2091
+
+},
+]
 const products=[
     {
         id:1 ,
@@ -85,6 +167,7 @@ const productTwo=[
        price: 1500
     },
 ]
+
 /** home-page (palace-memories section )/ sec-1 */
 
     const palaceSec = document.querySelector(".PalaceMemoirs-sec-products");
@@ -127,16 +210,63 @@ const productTwo=[
 
 
 
+    // gift-page//
+    let divs=[]
+    const productDetailSec=document.querySelector(".product-detail")
+    const gridOne=document.querySelector(".grid-1")
+    const gridTwo=document.querySelector(".grid-2")
+    function showGiftProducts(){
+        giftSection.forEach((gift)=>{
+            let div=document.createElement("div")
+            div.classList.add("product-page-detail")
+            div.innerHTML=`
+                <img src="${gift.img1}" >
+                <p>${gift.title}</p>
+                <p> MRP: $ <small>${gift.price}</small></p>
+                <button onClick="detail(${gift.id})">PRODUCT DETAIL</button>
+            `;
+            if(productDetailSec){
+                productDetailSec.append(div)
+                divs.push(div)
+            }
+        })
+    }
+    showGiftProducts()
 
-
+// grid-event-listener//
+gridTwo.addEventListener("click",()=>{
+ if(gridTwo){
+     divs.forEach((divv)=>{
+    return divv.style.width="300px"
+  })
+    gridTwo.style.backgroundColor="rgb(231, 220, 220)"
+   
+        gridOne.style.backgroundColor="white"
+ }
+  
+})
+gridOne.addEventListener("click",()=>{
+ if(gridOne){
+     divs.forEach((divv)=>{
+    return divv.style.width="450px"
+  })
+    gridOne.style.backgroundColor="rgb(231, 220, 220)"
+    
+        gridTwo.style.backgroundColor="white"
+    
+ }
+  
+})
 // --- cartpage.html pe ---
 const params = new URLSearchParams(window.location.search);
   const idData = parseInt(params.get("id"));
   
 
-  const allProducts = [...products, ...productTwo];
+  const allProducts = [...products, ...productTwo, ...giftSection];
   const findProduct = allProducts.find(item => item.id === idData);
   const container = document.querySelector(".product-detail-container");
+  const recentViewed = document.querySelector(".recent-viewed");
+  const recentViewedContent = document.querySelector(".recent-viewed-content");
 
  
         //<button onclick="addCart(${findProduct.id})">Add to Cart</button>//
@@ -146,6 +276,7 @@ const params = new URLSearchParams(window.location.search);
         div.classList.add("selected-product-details")
       div.innerHTML = `
        <div class="product-containers">
+       <div class="image-container-description">
        <div class="img-container">
        <div class="side-imgs">
         <img src="${findProduct.img1}" width="60px">
@@ -158,8 +289,31 @@ const params = new URLSearchParams(window.location.search);
        <img src="${findProduct.img1}" width="600">
        </div>
        </div>
+
+       <div class="product-description-and-information-sec">
        
+       <div class="description">
+       <p>Description</p>
+        <img src="/Chumbak-frontend-backend/chumbal-images/plus-icon" width="20px" alt="">
        </div>
+
+       <div class="product-information">
+       <p>Information</p>
+        <img src="/Chumbak-frontend-backend/chumbal-images/plus-icon" width="20px" alt="">
+       </div>
+
+       <div class="shipping-information">
+       <p>Shipping Information</p>
+        <img src="/Chumbak-frontend-backend/chumbal-images/plus-icon" width="20px" alt="">
+       </div>
+       <div class="returns">
+       <p>Returns</p>
+        <img src="/Chumbak-frontend-backend/chumbal-images/plus-icon" width="20px" alt="">
+       </div>
+
+       </div>
+       </div>
+       
        
 
        
@@ -198,6 +352,27 @@ const params = new URLSearchParams(window.location.search);
           </div>
           <p class="next-day-delivery"> Next day delivery is available in selected locations!</p>
         </div>
+        
+        <p class="share">Share</p>
+
+        <div class="offer-box">
+        <p class="best-offer-text"> Best offers for you!</p>
+
+        <div class="extra-50">
+        <p class="text-50-child1">Get EXTRA 15% OFF on your order | Code: DIWALI15</p>
+        <p class="text-50-child2">Valid on a min. purchase of ₹2999</p>
+        </div>
+
+        <div class="extra-offer">
+        <p class="extra-offer-child1">
+        Get ₹200 OFF on your 1st order | Code: CHUMBAKNEW
+        </p>
+        <p  class="extra-offer-child2" >Valid on a min. purchase of ₹2000</p>
+        </div>
+
+        </div>
+
+
         <div class="btns">
          <button onClick="addCart(${findProduct.id})" class="addCart"> ADD TO CART</button>
          <button  class="buyNow"> Buy Now</button>
@@ -205,6 +380,7 @@ const params = new URLSearchParams(window.location.search);
        </div>
        
       `;
+       
       if(container){
         container.appendChild(div)
       }
@@ -213,19 +389,70 @@ const params = new URLSearchParams(window.location.search);
     }
   }
 
-  //-- addCart function--//
+  // recent- viewed-sec//
+  let productDatas=[]
+ const recentViewProduct=allProducts.find((product)=>{
+    return product.id===idData
+ })
+
+
+productDatas=JSON.parse(localStorage.getItem("recentViewedItems"))||[]
+
+if(recentViewProduct){
+    productDatas.push(recentViewProduct)
+  }
+
+  if(productDatas.length>=5){
+        productDatas.splice(0, productDatas.length-5)
+     }
+       localStorage.setItem("recentViewedItems", JSON.stringify(productDatas))
+productDatas.forEach((pro)=>{
+            const div=document.createElement("div")
+            div.classList.add("recent-viewed-sec")
+    div.innerHTML = `
+                <img src="${pro.img1}" alt="">
+                <p>${pro.title}</p>
+                <p> MRP: $ <small>${pro.price}</small></p>
+                <button onClick="detail(${pro.id})">PRODUCT DETAIL</button>
+            `;
+    if(recentViewedContent){
+        recentViewedContent.appendChild(div)
+    }
+  })
+ 
+   
+
+
+  // wheel-event-section//
+   const imageContainerDescription = document.querySelector(".image-container-description");
+
+if(imageContainerDescription){
+    imageContainerDescription.addEventListener("wheel", (e) => {
+  e.preventDefault();
+  imageContainerDescription.scrollTop += e.deltaY; // moves vertically
+});
+}
+
+
+    //-- addCart function--//
 let cart=[]
 let cartCount=0
 
 const countCart=document.querySelector(".cart-count")
 
 function addCart(id){
-    
+    // store cart-data on localstorage//
+    // getItems or return []//
+      
+   console.log(cart)
+   //
      if(findProduct.id===id){
         findProduct.quantity=1
          cart.push(findProduct)
       }
-   console.log(cart)
+
+      // setItem on localstorege//
+    
     countCart.innerText = cart.length;
     if(cart.length > 0) addCartIconPara.style.backgroundColor = "red";
     showProduct();
@@ -290,6 +517,7 @@ function deleteItems(id){
       if(cartCount<0){
          countCart.innerText="0"
       }
+   
      showProduct()
       //--//
       if(countCart.innerText==="0"){
@@ -303,6 +531,7 @@ function increase(id){
        if(item.id===id){
         item.quantity+=1
        }
+       
    })
    showProduct()
 }
@@ -319,12 +548,19 @@ function decrease(id){
                 items.quantity=1
                
             }
+           
         })
+
     showProduct()
 }
   
 
-
+const backIcn=document.querySelector(".back-icn")
+if(backIcn){
+    backIcn.addEventListener("click",()=>{
+    window.location.href=`homepage.html`
+})
+}
 
 function detail(id){
  window.location.href=`cartpage.html?id=${id}`
